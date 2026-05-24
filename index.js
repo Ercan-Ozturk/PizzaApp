3e5a66fbf776cc460521931eef6742a3ed91dbcb
Ercan-Ozturk <ercanozturk788@gmail.com>
2026-05-24 15:33:17 +0300

    Project init

---
+++ index.js
@@ -0,0 +1,7 @@
+const menu = [
+  { name: "Margherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Vegetarian", price: 9 },
+  { name: "BBQ Chicken", price: 11 },
+  { name: "Hawaiian", price: 10 },
+];
